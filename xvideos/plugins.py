838e41089ff330f1263658b2be01fb10c684206a
Maike Mota <maike_henrique@hotmail.com>
2018-02-06 22:47:03 -0200

    Added support to request the luck for another person

--- xvideos/plugins.py
+++ xvideos/plugins.py
@@ -1,27 +1,27 @@
 
 import re
-from slackbot.bot import Bot, respond_to, listen_to
+from slackbot.bot import respond_to, listen_to
 from xvideos.xvideos import choose_random_porn_comment
 
-@respond_to('sorte do dia$', re.IGNORECASE)
-def sorte_do_dia(self, message):
-    comment = choose_random_porn_comment()
-    message.reply(format_sorte_de_hoje(comment.content))
+@respond_to(r'\Asorte ([\w\s]*)\Z', re.IGNORECASE)
+def sorte(message, tipo_sorte):
     # react with emoji
-    # message.react(':ok_hand::skin-tone-3:')
-
-@listen_to('sorte do dia para (.*)$', re.IGNORECASE)
-def sorte_do_dia_para(self, message, target):
+    message.react('ok_hand::skin-tone-3')
     comment = choose_random_porn_comment()
-    print(target)
-    print(comment)
-    self.say(format_sorte_de_hoje_para(target, comment.content))
+    formated_message = format_sorte(tipo_sorte, comment[1])
+    message.reply(formated_message)
+
+@respond_to(r'\Asorte ([\w\s]*) para (.*)\Z', re.IGNORECASE)
+def sorte_para(message, tipo_sorte, alvo_sorte):
+    message.react('ok_hand::skin-tone-3')
+    comment = choose_random_porn_comment()
+    message.reply(format_sorte_para(tipo_sorte, alvo_sorte, comment[1]))
 
 
-def format_sorte_de_hoje(content):
-    mask = 'Sua sorte de hoje é: "{0}"'
-    return mask.format(content)
+def format_sorte(tipo_sorte, content):
+    mask = 'Sua sorte {0} é: "{1}"'
+    return mask.format(tipo_sorte, content)
 
-def format_sorte_de_hoje_para(target, content):
-    mask = '{0} sua sorte para hoje é: "{1}"'
-    return mask.format(target, content)
+def format_sorte_para(tipo_sorte, alvo_sorte, content):
+    mask = 'Aqui está a sorte {0} para {1}: "{2}"'
+    return mask.format(tipo_sorte, alvo_sorte, content)
